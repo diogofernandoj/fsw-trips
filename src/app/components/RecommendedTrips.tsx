6a0f1cc2efc811d0bf6717c5605e6373f6912a14
Diogo Jorge <diogofernandoj@gmail.com>
2023-08-15 13:59:17 -0300

    feat: add reccomended trips component

---
+++ src/app/components/RecommendedTrips.tsx
@@ -0,0 +1,32 @@
+import TripItem from "@/components/TripItem";
+import { prisma } from "@/lib/prisma";
+
+const getTrips = async () => {
+  const trips = await prisma.trip.findMany({});
+
+  return trips;
+};
+
+const RecommendedTrips = async () => {
+  const trips = await getTrips();
+
+  return (
+    <div className="w-full max-w-6xl mx-auto p-5">
+      <div className="flex items-center">
+        <div className="w-full h-[1px] bg-grayLight"></div>
+        <h2 className="font-medium text-grayPrimary px-5 whitespace-nowrap">
+          Destinos recomendados
+        </h2>
+        <div className="w-full h-[1px] bg-grayLight"></div>
+      </div>
+
+      <div className="flex flex-col items-center mt-5 gap-8">
+        {trips.map((trip) => (
+          <TripItem key={trip.id} trip={trip} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RecommendedTrips;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import QuickSearch from "./components/QuickSearch";
+import RecommendedTrips from "./components/RecommendedTrips";
 import TripSearch from "./components/TripSearch";
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
     <div>
       <TripSearch />
       <QuickSearch />
+      <RecommendedTrips />
     </div>
   );
 };

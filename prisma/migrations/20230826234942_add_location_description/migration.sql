9a1d14fc513dad9c57b1d655131c3c95edc2de1d
Diogo Jorge <diogofernandoj@gmail.com>
2023-08-29 10:02:32 -0300

    feat: add location description

---
+++ prisma/migrations/20230826234942_add_location_description/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Trip" ADD COLUMN     "locationDescription" TEXT NOT NULL DEFAULT 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae laudantium explicabo animi rem numquam accusantium minima magnam a repellendus. Fugiat?';

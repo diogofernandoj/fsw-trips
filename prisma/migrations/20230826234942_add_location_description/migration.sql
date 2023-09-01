a4bfa7ae4b74aaf4bd5a08e9e9f49ea6f50c2046
Diogo Jorge <diogofernandoj@gmail.com>
2023-09-01 19:10:09 -0300

    feat: prisma db seed

--- prisma/migrations/20230826234942_add_location_description/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Trip" ADD COLUMN     "locationDescription" TEXT NOT NULL DEFAULT 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae laudantium explicabo animi rem numquam accusantium minima magnam a repellendus. Fugiat?';

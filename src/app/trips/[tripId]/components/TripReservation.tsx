7bb8824953850863840783caad7233f7445ea5cb
Diogo Jorge <diogofernandoj@gmail.com>
2023-09-02 14:12:27 -0300

    feat: add date validation to trip reservation

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -4,37 +4,41 @@ import Button from "@/components/Button";
 import DatePicker from "@/components/DatePicker";
 import Input from "@/components/Input";
 import { differenceInDays } from "date-fns";
+import { useRouter } from "next/navigation";
+import React from "react";
 import { Controller, useForm } from "react-hook-form";
 
-type TripReservationProps = {
+interface TripReservationProps {
   tripId: string;
   tripStartDate: Date;
   tripEndDate: Date;
   maxGuests: number;
   pricePerDay: number;
-};
+}
 
-type TripReservationForm = {
+interface TripReservationForm {
   guests: number;
   startDate: Date | null;
   endDate: Date | null;
-};
+}
 
 const TripReservation = ({
   tripId,
+  maxGuests,
   tripStartDate,
   tripEndDate,
-  maxGuests,
   pricePerDay,
 }: TripReservationProps) => {
   const {
     register,
     handleSubmit,
-    control,
     formState: { errors },
+    control,
     watch,
     setError,
   } = useForm<TripReservationForm>();
+
+  const router = useRouter();
 
   const onSubmit = async (data: TripReservationForm) => {
     const response = await fetch("/api/trips/check", {
@@ -51,14 +55,34 @@ const TripReservation = ({
     if (res?.error?.code === "TRIP_ALREADY_RESERVED") {
       setError("startDate", {
         type: "manual",
-        message: "Essa data ja está reservada.",
+        message: "Esta data já está reservada.",
       });
 
-      setError("endDate", {
+      return setError("endDate", {
         type: "manual",
-        message: "Essa data ja está reservada.",
+        message: "Esta data já está reservada.",
       });
     }
+
+    if (res?.error?.code === "INVALID_START_DATE") {
+      return setError("startDate", {
+        type: "manual",
+        message: "Data inválida.",
+      });
+    }
+
+    if (res?.error?.code === "INVALID_END_DATE") {
+      return setError("endDate", {
+        type: "manual",
+        message: "Data inválida.",
+      });
+    }
+
+    router.push(
+      `/trips/${tripId}/confirmation?startDate=${data.startDate?.toISOString()}&endDate=${data.endDate?.toISOString()}&guests=${
+        data.guests
+      }`
+    );
   };
 
   const startDate = watch("startDate");
@@ -66,87 +90,85 @@ const TripReservation = ({
 
   return (
     <div className="flex flex-col px-5">
-      <div className="flex gap-2">
+      <div className="flex gap-4">
         <Controller
           name="startDate"
           rules={{
             required: {
               value: true,
-              message: "A data de início é obrigatória.",
+              message: "Data inicial é obrigatória.",
             },
           }}
           control={control}
           render={({ field }) => (
             <DatePicker
-              className="w-full"
-              placeholderText="Data de início"
-              onChange={field.onChange}
               error={!!errors?.startDate}
               errorMessage={errors?.startDate?.message}
+              onChange={field.onChange}
               selected={field.value}
+              placeholderText="Data de Início"
+              className="w-full"
               minDate={tripStartDate}
               maxDate={tripEndDate}
             />
           )}
         />
+
         <Controller
           name="endDate"
           rules={{
             required: {
               value: true,
-              message: "A data final é obrigatória.",
+              message: "Data final é obrigatória.",
             },
           }}
           control={control}
           render={({ field }) => (
             <DatePicker
-              className="w-full"
-              placeholderText="Data final"
-              onChange={field.onChange}
               error={!!errors?.endDate}
               errorMessage={errors?.endDate?.message}
+              onChange={field.onChange}
               selected={field.value}
-              minDate={startDate ?? tripStartDate}
+              placeholderText="Data Final"
+              className="w-full"
               maxDate={tripEndDate}
+              minDate={startDate ?? tripStartDate}
             />
           )}
         />
       </div>
+
       <Input
         {...register("guests", {
           required: {
             value: true,
-            message: "O número de hóspedes é obrigatório.",
+            message: "Número de hóspedes é obrigatório.",
           },
           max: {
             value: maxGuests,
-            message: `Número de hóspedes não pode ser maior que ${maxGuests}`,
+            message: `Número de hóspedes não pode ser maior que ${maxGuests}.`,
           },
         })}
-        className="mt-2"
-        placeholder={`Número de hóspedes (máx: ${maxGuests})`}
+        placeholder={`Número de hóspedes (max: ${maxGuests})`}
+        className="mt-4"
         error={!!errors?.guests}
-        errorMessage={errors.guests?.message}
+        errorMessage={errors?.guests?.message}
         type="number"
       />
 
       <div className="flex justify-between mt-3">
-        <p className="font-medium text-sm text-primaryDark">{`Total${
-          startDate && endDate
-            ? "(" + differenceInDays(endDate, startDate) + " dias)"
-            : ""
-        }: `}</p>
-        <p className="font-medium text-sm text-primaryDark">{`${
-          startDate && endDate
-            ? differenceInDays(endDate, startDate) * pricePerDay
-            : "R$0"
-        }`}</p>
+        <p className="font-medium text-sm text-primaryDark">Total: </p>
+        <p className="font-medium text-sm text-primaryDark">
+          {startDate && endDate
+            ? `R$${differenceInDays(endDate, startDate) * pricePerDay}` ?? 1
+            : "R$0"}
+        </p>
       </div>
 
-      <div className="pb-10 border-b border-grayLight w-full">
+      <div className="pb-10 border-b border-b-grayLight w-full lg:border-none lg:pb-0">
         <Button
-          className="mt-3 w-full"
           onClick={() => handleSubmit(onSubmit)()}
+          className="mt-3 w-full"
         >
           Reservar agora
         </Button>

90063e0d74b0f8435a3d1de886686ac3fa5531e0
Diogo Jorge <diogofernandoj@gmail.com>
2023-09-05 12:04:10 -0300

    feat: add config pre deploy

--- src/app/api/payment/route.ts
+++ src/app/api/payment/route.ts
@@ -19,7 +19,7 @@ export async function POST(request: Request) {
   } = await request.json();
 
   const session = await stripe.checkout.sessions.create({
-    success_url: "http://localhost:3000",
+    success_url: process.env.HOST_URL!,
     metadata: {
       tripId,
       userId,

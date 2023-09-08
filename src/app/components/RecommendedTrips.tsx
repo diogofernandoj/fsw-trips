a9612d4f5d08803c2ef4a26f267342cfd2fb4d7e
Diogo Jorge <diogofernandoj@gmail.com>
2023-09-08 14:09:44 -0300

    style: adapt recommended trips to desktop

--- src/app/components/RecommendedTrips.tsx
+++ src/app/components/RecommendedTrips.tsx
@@ -14,13 +14,13 @@ const RecommendedTrips = async () => {
     <div className="w-full max-w-6xl mx-auto p-5">
       <div className="flex items-center">
         <div className="w-full h-[1px] bg-grayLight"></div>
-        <h2 className="font-medium text-grayPrimary px-5 whitespace-nowrap">
+        <h2 className="font-medium text-grayPrimary px-5 whitespace-nowrap lg:text-base">
           Destinos recomendados
         </h2>
         <div className="w-full h-[1px] bg-grayLight"></div>
       </div>
 
-      <div className="flex flex-col items-center mt-5 gap-8">
+      <div className="flex flex-col items-center mt-5 gap-8 lg:flex-row lg:flex-wrap lg:justify-center lg:mt-10">
         {trips.map((trip) => (
           <TripItem key={trip.id} trip={trip} />
         ))}

7d5254e6ef65d9edf3e5d8e2d3ee8c3f81849799
Diogo Jorge <diogofernandoj@gmail.com>
2023-09-08 14:09:03 -0300

    style: adapt trip header to desktop

--- src/app/trips/[tripId]/components/TripHeader.tsx
+++ src/app/trips/[tripId]/components/TripHeader.tsx
@@ -9,7 +9,7 @@ type TripHeaderProps = {
 const TripHeader = ({ trip }: TripHeaderProps) => {
   return (
     <div className="flex flex-col">
-      <div className="relative h-[300px] w-full">
+      <div className="relative h-[300px] w-full lg:hidden">
         <Image
           src={trip.coverImage}
           alt={trip.name}
@@ -18,15 +18,81 @@ const TripHeader = ({ trip }: TripHeaderProps) => {
         />
       </div>
 
-      <div className="flex flex-col p-5">
-        <h1 className="font-semibold text-xl text-primaryDark">{trip.name}</h1>
-
-        <div className="flex items-center gap-1">
-          <ReactCountryFlag countryCode={trip.countryCode} svg />
-          <p className="text-xs text-grayPrimary underline">{trip.location}</p>
+      <div className="hidden lg:grid grid-cols-[2fr,1fr,1fr] gap-2 grid-rows-2 lg:order-2">
+        <div className="relative row-span-2">
+          <Image
+            src={trip.coverImage}
+            fill
+            style={{
+              objectFit: "cover",
+            }}
+            alt={trip.name}
+            className="rounded-tl-lg rounded-bl-lg shadow-md"
+          />
         </div>
 
-        <p className="text-xs text-grayPrimary">
+        <div className="relative h-[200px] w-full">
+          <Image
+            src={trip.imagesURL[0]}
+            fill
+            style={{
+              objectFit: "cover",
+            }}
+            alt={trip.name}
+            className="shadow-md"
+          />
+        </div>
+
+        <div className="relative h-[200px] w-full">
+          <Image
+            src={trip.imagesURL[1]}
+            fill
+            style={{
+              objectFit: "cover",
+            }}
+            alt={trip.name}
+            className="shadow-md  rounded-tr-lg"
+          />
+        </div>
+
+        <div className="relative h-[200px] w-full">
+          <Image
+            src={trip.imagesURL[2]}
+            fill
+            style={{
+              objectFit: "cover",
+            }}
+            alt={trip.name}
+            className="shadow-md"
+          />
+        </div>
+
+        <div className="relative h-[200px] w-full">
+          <Image
+            src={trip.coverImage}
+            fill
+            style={{
+              objectFit: "cover",
+            }}
+            alt={trip.name}
+            className="shadow-md  rounded-br-lg"
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-col p-5 lg:p-0 lg:mb-5">
+        <h1 className="font-semibold text-xl text-primaryDark lg:text-3xl">
+          {trip.name}
+        </h1>
+
+        <div className="flex items-center gap-1 my-1">
+          <ReactCountryFlag countryCode={trip.countryCode} svg />
+          <p className="text-xs text-grayPrimary underline lg:text-base">
+            {trip.location}
+          </p>
+        </div>
+
+        <p className="text-xs text-grayPrimary lg:hidden">
           <span className="text-primary font-medium">
             R${trip.pricePerDay.toString()}
           </span>{" "}

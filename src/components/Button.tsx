711364edbe61af004dfbb73b6ca2f495f2717183
Diogo Jorge <diogofernandoj@gmail.com>
2023-09-11 18:16:09 -0300

    feat: add loading variant to button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ import { ComponentPropsWithoutRef } from "react";
 import { twMerge } from "tailwind-merge";
 
 interface ButtonProps extends ComponentPropsWithoutRef<"button"> {
-  variant?: "primary" | "outlined" | "danger";
+  variant?: "primary" | "outlined" | "danger" | "loading";
 }
 
 function Button({ className, variant = "primary", ...props }: ButtonProps) {
@@ -12,17 +12,22 @@ function Button({ className, variant = "primary", ...props }: ButtonProps) {
       "bg-transparent border border-primary text-primary hover:bg-primary hover:text-white",
     danger:
       "text-red-500 border-red-500 border hover:bg-red-600 bg-transparent hover:text-white",
+    loading: "bg-primaryDark cursor-not-allowed",
   };
 
   const _className = twMerge(
     variantClasses[variant],
-    "appearance-none rounded-lg p-2 text-sm font-medium shadow transition-all",
+    "rounded-lg p-2 text-sm font-medium transition-all flex justify-center items-center",
     className
   );
 
   return (
     <button className={_className} {...props}>
-      {props.children}
+      {variant !== "loading" ? (
+        props.children
+      ) : (
+        <div className="animate-spin h-5 w-5 border-[3px] border-primaryLight border-t-white rounded-full"></div>
+      )}
     </button>
   );
 }
